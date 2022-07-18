9caf22ef87b400d1ce61c45c2bddeadc94b55173
Yassir56069 <hoossanbukshyassir@gmail.com>
2022-07-18 19:31:53 +0400

    added an error message when 3parameters are given to python script.

--- addTwoInt.py
+++ addTwoInt.py
@@ -5,8 +5,11 @@ import sys
 def main():
 	a = (sys.argv[1])
 	b = (sys.argv[2])
-	add(a, b)
 
+	if (sys.argv[3] != ""):  #if it exists
+		print("error! more than 2 arguments given..")
+	else:
+		add(a,b)
 def add(a, b):
 	c = int(a)  + int(b)
 	print(c)
